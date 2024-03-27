fbda93fb0969eb565f77178a751330f2746f08ad
Wanessa Santos <145464872+WanessaMSantos@users.noreply.github.com>
2024-03-27 16:40:10 -0300

    Update scripts.js
    Configurando carrossel

--- Site/scripts.js
+++ Site/scripts.js
@@ -30,4 +30,28 @@ document.getElementById('postForm').addEventListener('submit', function(e) {
       document.getElementById('postImage').value = '';
     }
   });
+
+//Configurando Carrossel
+  
+  var radio = document.querySelector('.manual-btn')
+
+  var cont = 1
+
+  document.getElementById('radio1').checked = true
+
+  setInterval(() => {
+    proximaImg()
+  }, 5000)
+
+  function proximaImg(){
+    cont++
+
+    if(cont > 3){
+      cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+
+
+  }
   
