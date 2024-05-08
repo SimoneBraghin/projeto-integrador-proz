d84b24e813d316d79dd34a2b9c231f615a0e285f
lenilsonfigueiredobr <lenilson.sistema@gmail.com>
2024-05-08 08:42:54 -0300

    Update scripts.js

--- Site/scripts.js
+++ Site/scripts.js
@@ -3,13 +3,32 @@ document.getElementById('postForm').addEventListener('submit', function(e) {
     const text = document.getElementById('postText').value;
     const image = document.getElementById('postImage').files[0];
     
+    // if (text.trim() !== '' || image) {
+    //   // Criar elemento de postagem
+    //   const post = document.createElement('div');
+    //   post.classList.add('post');
+    //   const postContent = `
+    //     <div class="post-header">
+    //       <img src="profile-pic.jpg" alt="Profile Picture">
+    //       <div class="post-info">
+    //         <h2>Nome do Usuário</h2>
+    //         <p>@usuario</p>
+    //       </div>
+    //     </div>
+    //     <div class="post-content">
+    //       <p>${text}</p>
+    //       ${image ? `<img src="${URL.createObjectURL(image)}" alt="Post Image">` : ''}
+    //     </div>
+    //   `;
+    //   post.innerHTML = postContent;
+
     if (text.trim() !== '' || image) {
       // Criar elemento de postagem
       const post = document.createElement('div');
       post.classList.add('post');
       const postContent = `
         <div class="post-header">
-          <img src="profile-pic.jpg" alt="Profile Picture">
+          
           <div class="post-info">
             <h2>Nome do Usuário</h2>
             <p>@usuario</p>
